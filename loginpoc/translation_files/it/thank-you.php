7f208c2be458455b0545e7631dfc75153d34e940
Ayush Agrawal <agrawalayush92@gmail.com>
2016-10-05 00:17:28 +0530

    it trans added

---
+++ loginpoc/translation_files/it/thank-you.php
@@ -0,0 +1,11 @@
+<?php
+	$translations = array(
+    'labels' =>
+        array(
+        	"Thank You!"=>"Grazie",
+        	"Any thank you text can come over here. This is the space for any dummy text.This is a place holder for the text to be filled."=>"Any thank you text can come over here. This is the space for any dummy text.This is a place holder for the text to be filled.",
+        	"Your account has now been fully transitioned to ETX Capital and a member of the ETX team will be in contact with you shortly." => "Il tuo conto è stato pienamente trasferito a ETX Capital ed un membro del team ETX sarà in presto in contatto con te.",
+        	"From everyone here at PipIndex, we thank you and wish you all the best." => "Un ringraziamento ed un augurio speciale da parte dello staff di PipIndex",
+    	)
+    );
+?>
