0bbf72de1c161c02314913c25d77517034baa997
Ayush Agrawal <agrawalayush92@gmail.com>
2016-10-05 20:41:34 +0530

    tnc trans

--- loginpoc/new_includes/translation_files/it/terms-and-conditions.php
+++ loginpoc/new_includes/translation_files/it/terms-and-conditions.php
@@ -1,17 +1,116 @@
 <?php
-	$translations = array(
-    'labels' =>
-        array(
-        	"tncTitle"=>"Termini e Condizioni",
-            "tncMessage"=>"By choosing “I agree” below you agree to Pipindex’s Terms and Conditions.<br>
-		You also agree to our Privacy Policy, which describes how we process your information, including these key points. This is dummy text and needs to be replaced.",
-			"dataProcessTitle"=>"Data we process when you use Pipindex",
-			"dataProcessList1"=>"This is a dummy text and will be replaced by the original text to be provided at a later stage. These are major bullet points under terms and conditions.",
-			"dataProcessList2"=>"This is a dummy text and will be replaced by the original text to be provided at a later stage. These are major bullet points under terms and conditions.",
-			"dataProcessList3"=>"This is a dummy text and will be replaced by the original text to be provided at a later stage. These are major bullet points under terms and conditions.",
-			"Depending on your account settings, some of this data may be associated with your Pipindex Account and we treat this data as personal information. This is a dummy text and needs to change to original"=>"Depending on your account settings, some of this data may be associated with your Pipindex Account and we treat this data as personal information. This is a dummy text and needs to change to original",
-			"CANCEL"=>"Annulla",
-			"I AGREE"=>"Accetto"
-    	)
+    $translations = array(
+        "en"=>array(
+            "text" => array(
+                "agree"=>"I Agree",
+                "cancel"=> "Cancel"
+            ),
+            "title"=>"Terms & Condition | PipIndex Capital Markets",
+            "body" => array(
+                'header' => array('' => "" ),
+                'content' => array(
+                    'title' => "Terms and Conditions",
+                    'message'=>"By clicking “I agree” you agree to PipIndex passing your personal information to ETX Capital, so that ETX Capital can contact you via phone and/or email and you agree to the Privacy Policy of ETX Capital as below.",
+                    'terms' => array(
+                        array(
+                            "header"=>"ETX Privacy policy",
+                            "para"=> array(
+                                "ETX Capital recognises the vital importance of protecting your personal and financial information and adheres to the principles of the Data Protection Act 1998 (the “Act”). This enables us to provide you with a secure website for your transactions. This privacy policy details how ETX Capital ensures your personal information remains confidential, respects your privacy and safeguards your personal data at all times.",
+                                "Please read the following carefully to understand our views and practices regarding your personal data and how we will treat it. By visiting and/or accessing our website you are accepting and consenting to the practices described in this privacy policy."
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"1. COLLECTION OF PERSONAL DATA",
+                            "para"=> array(
+                                "You may give us information about you by using our website, filling in forms on our site or by corresponding with us by phone, e-mail or otherwise. This includes information you provide when you register to use our site, subscribe to our services and products and when you report a problem with our site. The kind of personal information you give us may include (but is not limited to) name, address, date of birth, passport number, tax identification number, e-mail address, phone number, employment and income, debit/credit card information, transaction history and financial statements. In order to comply with our regulatory requirements, we may also ask for financial information such as annual income, net worth and available risk capital. In addition, we may also collect information from external sources."
+                            ),
+                            "list"=>array(
+                                "to verify your identity;",
+                                "to assess the appropriateness of the products and services we provide;",
+                                "to set up and service your account;",
+                                "to carry out our services and to provide you with the information, products and services that you request from us;",
+                                "to provide you with information about other products and services we offer that are similar to those that you have already enquired about;",
+                                "to notify you about changes to our services and products;",
+                                "to improve our website."
+                            )
+                        ),
+                        array(
+                            "header"=>"2. COOKIES",
+                            "para"=> array(
+                                "We use cookies to provide us with data we can use to improve the website experience for our customers and to get to know our customers better. For detailed information on the cookies we use and the purposes for which we use them visit our Cookie page."
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"3. USE OF PERSONAL DATA",
+                            "para"=> array(
+                                "We use information held about you in the following ways:"
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"4. DISCLOSURE OF PERSONAL DATA",
+                            "para"=> array(
+                                "We do not sell, license or otherwise disclose your personal information to third parties for marketing purposes. However, we may, in the standard operation of our business, share selected information to the following (who may be within or outside the European Economic Area):"
+                            ),
+                            "list"=>array(
+                                "any relevant regulatory, governmental, or law enforcement authority as required by law;",
+                                "our third party credit/debit card processing and verification providers as required by our commercial banks;",
+                                "our insurers, agents, suppliers and subcontractors insofar as reasonably necessary for the purpose set out herein;",
+                                "professional advisors such as our solicitors in connection with any ongoing or prospective legal proceedings or in order to establish, exercise or defend our legal rights, and our auditors and accountants in order to satisfy our regulatory and financial reporting obligations;",
+                                "introducing brokers with whom we have a mutual relationship;",
+                                "any member of our group, meaning our subsidiaries, our ultimate holding company and its subsidiaries, and successors in title to our business."
+                            )
+                        ),
+                        array(
+                            "header"=>"5. SECURITY",
+                            "para"=> array(
+                                "ETX Capital takes the safeguarding of your data very seriously. ETX Capital has taken measures and precautions to protect the security of your personal information."
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"6. YOUR RIGHTS",
+                            "para"=> array(
+                                "You are not required to submit requested personal information. However, failure to disclose your personal information upon our request may result in our being unable to provide you with the requested product or service or to continue to maintain your account.",
+                                "Our site may, from time to time, contain links to and from the websites of our partner networks, advertisers and affiliates. If you follow a link to any of these websites, please note that these websites have their own privacy policies and that we do not accept any responsibility or liability for these policies. Please check these policies before you submit any personal data to these websites."
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"7. ACCESS TO INFORMATION",
+                            "para"=> array(
+                                "The Act gives you the right to access information held about you. Your right of access can be exercised in accordance with the Act. Any access request may be subject to a fee to meet our costs in providing you with details of the information we hold about you."
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"8. CHANGES TO OUR PRIVACY POLICY",
+                            "para"=> array(
+                                "Any changes we may make to our privacy policy in the future will be posted on this page and, where appropriate, notified to you by e-mail. Please check back frequently to see any updates or changes to our privacy policy."
+                            ),
+                            "list"=>array(
+                            )
+                        ),
+                        array(
+                            "header"=>"9. CONTACT",
+                            "para"=> array(
+                                "If you have any questions regarding our privacy policy, please email:."
+                            ),
+                            "list"=>array(
+                            )
+                        )                    
+                    )
+                ), 
+                'footer' => array('' => ""), 
+            )
+        )
     );
 ?>
