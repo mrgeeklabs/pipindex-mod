01f86d9c0954018bbb5155532f70e30e5204ba19
niranjan <niranjan.b@shawacademy.com>
2016-10-09 19:16:37 +0530

    fixed logo plus responsive issue

--- loginpoc/new_includes/utp_header.php
+++ loginpoc/new_includes/utp_header.php
@@ -7,7 +7,8 @@
 <header>
 	<div class="header-container nonfunded_header">
 		<h1><?= $translationsHeader[$lang][0]?> <span><?= $translationsHeader[$lang][1]?></span></h1>
-		<img src="new-static/images/utpLogo.svg" id="utpLogo" alt="no-image">		
+		<!--<img src="new-static/images/utpLogo.svg" id="utpLogo" alt="no-image">-->
+		<img src="new-static/images/logo3.svg" id="utpLogo" alt="no-image">
 		<div class="pipindex-lang">
 			<!--<img src="new-static/images/lang-icon.svg" style="width: 18px;height: 18px;color: #313756;opacity: 0.8;">
 			<span style="font-family: Open Sans;font-size: 11px;letter-spacing: 0.3px;color: #4fa4ce;">EN</span>-->
