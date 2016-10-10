adbd65193ee778316f083278b0b5094719bf1a1e
niranjan <niranjan.b@shawacademy.com>
2016-10-10 23:35:58 +0530

    footer changes

--- loginpoc/footer_utp.php
+++ loginpoc/footer_utp.php
@@ -1,44 +1,5 @@
 <footer>
     <div class="footer-container">
-        <!-- <div class="row footer-links">
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">                
-                <h5>About Us</h5>
-                <ul class="footer-links-arrow">
-                    <li><a href="http://www.academyft.com/about-academy-financial-trading-online.htm">About Academy FT</a></li>
-                    <li><a href="http://www.academyft.com/learn-how-to-be-a-trader-code-of-practice.htm">Code Of Practice</a></li>
-                    <li><a href="http://www.academyft.com/continuing-professional-development.htm">CPD</a></li>
-                    <li><a href="http://www.academyft.com/learn-how-to-trade-partnership-opportunities.htm">Partnership Opportunities</a></li>
-                    <li><a href="http://www.academyft.comhttp://www.academyft.com/company-seeking-finance.php">VENTURE CAPITAL</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <h5>Trading Courses</h5>
-                <ul class="footer-links-arrow">
-                    <li><a href="http://www.academyft.com/learn-how-to-be-a-trader-foundation.htm">Foundation Trading</a></li>
-                    <li><a href="http://www.academyft.com/learn-how-to-be-a-trader-ultimate.htm">Ultimate Trader</a></li>
-                    <li><a href="http://www.academyft.com/learn-how-to-trade-online-testimonials.htm">Testimonials and Awards</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <h5>Why Learn To Trade</h5>
-                <ul class="footer-links-arrow">
-                    <li><a href="http://www.academyft.com/why-learn-to-trade.htm">Why learn to trade</a></li>
-                    <li><a href="http://www.academyft.com/why-trend-trading-online.htm">Why trend trading</a></li>
-                    <li><a href="http://www.academyft.com/learn-how-to-trade-forex.htm">Trading in forex</a></li>
-                    <li><a href="http://www.academyft.com/learn-how-to-trade-online-money-management.htm">Money management</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <ul class="footer-links-wd-arrow">
-                    <li><a href="http://www.academyft.com/FAQ.php">Foundation FAQ</a></li>
-                    <li><a href="http://www.academyft.com/contact-academy-financial-trading-online.htm">Contact us</a></li>
-                    <li> <a href="http://www.academyft.com/complaint-form.php" target="_new">Complaint Form</a></li>
-                    <li><a href="http://www.academyft.com/terms.htm">Terms of Use</a></li>
-                    <li><a href="http://www.academyft.com/disclaimer.htm">Disclaimer</a></li>
-                    <li><a href="http://www.academyft.com/privacy-policy.htm">Privacy</a></li>
-                </ul>
-            </div>
-        </div> -->
         <div class="footer-tagline">
             <span style="color: #97dbe3;">© 2016</span> | <b><span style="color:#fff;">Academy of Financial Trading.</span></b> All Rights Reserved.
         </div>
@@ -48,6 +9,8 @@
             Disclaimer: Any opinions, news, research, analysis, prices, or other information contained on this website or any other material provide by the Academy of Financial Trading and associated companies or employees is provided as general market commentary, and does not constitute investment advice or a solicitation to buy or sell any foreign exchange contract, contract for difference or securities of any type - It does not take into account your personal circumstances, please do not trade or invest based solely on this information. By Viewing any material or using the information within this site you agree that this is general education material and you will not hold any person or entity responsible for loss or damages resulting from the content or general information provided here by The Academy of Financial Trading, it's employees, directors or fellow members. Futures, options, and spot currency trading have large potential rewards, but also large potential risk. You must be aware of the risks and be willing to accept them in order to invest in the futures, foreign exchange and options markets. Don't trade with money you can't afford to lose. This website is neither a solicitation nor an offer to Buy/Sell futures, spot forex, cfd's, options or other financial products. No representation is being made that any account will or is likely to achieve profits or losses similar to those discussed in any material on this website. The past performance of any trading system or methodology is not necessarily indicative of future results. For the avoidance of any doubt, the Academy of Financial Trading and any associated companies, or employees, do not hold themselves out as Commodity Trading Advisors (CTAs). Given this representation, all information and material provided by the Academy of Financial Trading and any associated companies, or employees, is for educational purposes only and should not be considered specific investment advice.
             <br><br>
             High Risk Warning: Foreign Exchange, Futures, and Options trading has large potential rewards, but also large potential risks. The high degree of leverage can work against you as well as for you. You must be aware of the risks of investing in forex, futures, and options and be willing to accept them in order to trade in these markets. Forex trading involves substantial risk of loss and is not suitable for all investors. Please do not trade with borrowed money or money you cannot afford to lose. Any opinions, news, research, analysis, prices, or other information contained on this website is provided as general market commentary and does not constitute investment advice. We will not accept liability for any loss or damage, including without limitation to, any loss of profit, which may arise directly or indirectly from the use of or reliance on such information. Please remember that the past performance of any trading system or methodology is not necessarily indicative of future results. 
+            <br>
+            £10,000 Prize Terms and Conditions: Only applicable to PipIndex Capital Markets customers that transfer the balance of their  trading accounts to ETX Capital and place an initial trade during the promotion period specified above, or to customers who upgrade from demo to live account during the period and place their first trade with ETX Capital. The rules are simple but we must say them out loud; upgrade your account, place your trade to enter the competition and you might be drawn as the lucky winner. Cash prize will only be credited to the customer who wins for AML reasons hence the funds will only be credited to an account in the name matching our records. The cash prize will be sent as £10,000. Should there be any foreign exchange or other bank charges such will be the responsibility of the fortunate recipient. The deadlines for the first trade prize are given with ample time hence neither PipIndex, nor ETX Capital, nor Finsa Europe Limited accept any responsibility for delays in opening or closing trading accounts, in remitting funds, or otherwise allowing a trade deal to be placed so as the customer is eligible to meet the terms and conditions of this prize draw. Any changes to the above Terms and Conditions are at the discretion of PipIndex Capital Markets.
         </div>    
    </div> 
 </footer>
