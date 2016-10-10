091a6c4e396d80079659a008ac7a057db85c7fc2
Ayush Agrawal <agrawalayush92@gmail.com>
2016-10-10 12:35:26 +0530

    terms and conditions link added

--- loginpoc/home-demo-utp.php
+++ loginpoc/home-demo-utp.php
@@ -69,7 +69,7 @@
 	                <p id="account-better"><?= $translations[$lang]["banner_right"][0] ?></p>
 	                <h2 id="welcome-title"><?= $translations[$lang]["banner_right"][1] ?></h2>
 	                <p id="welcome-text"><?= $translations[$lang]["banner_right"][2] ?></p>
-	                <a id="banner-button" href="https://www.etxcapital.com"><?= $translations[$lang]["text"]["open_your_account"]?></a>
+	                <a id="banner-button" href="terms-and-condition.php<?php echo (isset($_GET['email']) ? '?email=' .trim($_GET['email'])  : '') ?>"><?= $translations[$lang]["text"]["open_your_account"]?></a>
 	            </div>
 	        </div>
 	    </div>
@@ -110,7 +110,7 @@
 	            </div>
 	        </div>
 
-	        <a class="btn update-account-button" href="https://www.etxcapital.com"><?= $translations[$lang]["text"]["open_your_account"]?></a>
+	        <a class="btn update-account-button" href="terms-and-condition.php<?php echo (isset($_GET['email']) ? '?email=' .trim($_GET['email'])  : '') ?>"><?= $translations[$lang]["text"]["open_your_account"]?></a>
 	    </div>
 	    <div class="prize-container">
 	        <p id="prize-amount"><?= $priceMoney ?></p>
@@ -178,7 +178,7 @@
 	            <p class="feature-title"><?= $translations[$lang]["feature"][1] ?></p>
 	        </div>
 	        <div class="update-wrapper">
-	            <a class="btn update-account-button" href="https://www.etxcapital.com"><?= $translations[$lang]["text"]["open_your_account"]?></a>
+	            <a class="btn update-account-button" href="terms-and-condition.php<?php echo (isset($_GET['email']) ? '?email=' .trim($_GET['email'])  : '') ?>"><?= $translations[$lang]["text"]["open_your_account"]?></a>
 	        </div>
 	    </div>
 	    <div class="scholars-section-container">
@@ -207,7 +207,7 @@
 				?>
 
 	            <div class="col-md-12 col-sm-12">
-	                <a href="https://www.etxcapital.com" class="button"><?= $translations[$lang]["text"]["open_your_account"]?></a>
+	                <a href="terms-and-condition.php<?php echo (isset($_GET['email']) ? '?email=' .trim($_GET['email'])  : '') ?>" class="button"><?= $translations[$lang]["text"]["open_your_account"]?></a>
 	            </div>
 	        </div>
 	    </div>
@@ -302,7 +302,7 @@
 				}
 				?>
 	        </div>
-	        <a href="https://www.etxcapital.com" class="button"><?= $translations[$lang]["text"]["open_your_account"]?></a>
+	        <a href="terms-and-condition.php<?php echo (isset($_GET['email']) ? '?email=' .trim($_GET['email'])  : '') ?>" class="button"><?= $translations[$lang]["text"]["open_your_account"]?></a>
 	    </div>
 	    <div class="have-query">
 	        <p id="query-title"><?= $translations[$lang]["have-query"][0] ?></p>
