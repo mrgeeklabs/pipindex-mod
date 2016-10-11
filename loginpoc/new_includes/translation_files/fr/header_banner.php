b50b87cdc0991d26f3c9ecb8ad29a192f7fc676b
Ayush Agrawal <agrawalayush92@gmail.com>
2016-10-11 19:09:41 +0530

    fr lang banner and other pages

---
+++ loginpoc/new_includes/translation_files/fr/header_banner.php
@@ -0,0 +1,8 @@
+<?php
+    $translationsHeader = array(
+        "fr"=>array(
+            "OFFER EXPIRES",
+            "MONDAY 24th OCTOBER"
+        )
+    );
+?>
---
+++ loginpoc/new_includes/translation_files/fr/home-live-non-utp-funded.php
@@ -0,0 +1,94 @@
+<?php
+    $translations = array(
+        "en"=>array(
+            "text" => array(
+                "agree"=>"Je suis d'accord",
+                "cancel"=> "Annuler",
+                "watch_video" => "REGARDER LA VIDÉO",
+                "update_account" => "MISE À JOUR DE VOTRE COMPTE",
+                "open_your_account" => "OUVRIR VOTRE COMPTE",
+                "sell" => "Vendre",
+                "eurusd" => "EUR/USD",
+                "buy" => "Acheter",
+                "submit" => "SOUMETTRE",
+                "request_call" => "DEMANDE UN APPEL",
+                "thank_you" => "Je vous remercie",
+                "thank_you_mesage" => "Nous reviendrons vers vous sous peu",
+                "not_recognize" => "Nous ne pouvons pas vous reconnaître",
+                "not_recognize_text" => "Entrez vos coordonnées ci-dessous et tout est parfait !",
+                "please_contact_us" => "CONTACTEZ-NOUS S'IL VOUS PLAIT",
+                "table_header_deposite" => "DÉPÔT ",
+                "table_header_bonus" => "BONUS",
+                "table_header_october_bonus" => "BONUS d’octobre",
+            ),
+            "title"=>"Accueil | PipIndex Capital Markets",
+            "banner_right"=> array(
+                "Votre compte s’actualise !",
+                "BIENVENUE À ETX",
+                "Mettre à jour votre compte ci-dessous et recevoir le",
+                "PROGRAMME ULTIMATE TRADERS",
+                "Complètement gratuit de l’Academy of Financial Trading"
+            ),
+            "update-account"=> array(
+                "Mettre à jour votre compte et Placer 1 Trade d'ici le lundi 24e Octobre 2016 et participer à un tirage",
+                "pour gagner 10.000 $ en argent réel !",
+                "+ Programme ultime Traders gratuit"
+            ),
+            "update-steps-container"=> array(
+                array(
+                    "title" => "ÉTAPE 1",
+                    "info" => 'Cliquez sur « Mettre à jour votre compte"',
+                ),
+                array(
+                    "title" => "ÉTAPE 2",
+                    "info" => "Examiner les nouvelles Conditions"
+                ),
+                array(
+                    "title" => "ÉTAPE 3",
+                    "info" => "Mettre à jour quelques informations de base"
+                ),
+                array(
+                    "title" => "ÉTAPE 4",
+                    "info" => "Recevoir l’UTP gratuit"
+                )
+            ),
+            "prize-container" => array(
+                "PRIX DU PREMIER TRADE",
+                "Pour accéder à ce tirage au sort, il suffit de placer un Trade.",
+                "Le gagnant sera tiré au plus tard le lundi 24 Octobre à 18:00 BST"
+            ),
+            "new-bonus-title"=> "+ Nouveau Compte Bonus",
+            "features-container" => array(
+                "Les spreads aussi bas que 0,8",
+                "COÛTS TRANSACTIONNELS INFÉRIEURS SUR INSTRUMENTS POPULAIRES"
+            ),
+            "feature" => array(
+                "INSTRUMENTS NÉGOCIABLES",
+                "VASTE GAMME DE MARCHÉS"
+            ),
+            "scholars-section-container" => array(
+                "COMPTE ACTUALISE EN TOUTE TRANSPARENCE",
+                "RÉGLEMENTÉ PAR L'AUTORITÉ FINANCIÈRE DE UK",
+                "SUPPORT MANAGER DÉDIÉ",
+                "LANGUES INNOMBRABLES",
+                "PLATEFORME DE TRADING MT4 COMPLETE",
+                "GAMME COMPLÈTE DE PLATEFORMES MOBILES"
+            ),
+            "bullet-points" => array(
+                "Cliquez pour mettre à jour votre compte",
+                "Vous serez mis à niveau vers la plateforme de Trading MT4 de ETX",
+                "Placer 1 Trade et vous recevrez le Ultimate Traders Programme </ strong> gratuitement",
+                "Un membre de l’equipe de ETX vous appellera pour vous accueillir et vous offrir un soutien",
+                "L'éducation gratuite sera fournie sur MT4",
+                "Plus de fiabilité avec un plus grand soutien",
+                "Placer 1 Trade et vous pourrez gagner 10 000 $"
+            ),
+            "improve-account-container" => "Améliorer votre compte dès maintenant !",
+            "have-query" => array(
+                "Vous avez des questions ?",
+                "Choisissez un moment approprié et nous vous contacterons."
+            ),
+            "ask-question-section" => "Posez une question et nous reviendrons vers vous"
+        )
+    );
+?>
